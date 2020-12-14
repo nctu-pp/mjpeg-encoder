461c894900f9c87de44dc32b977f64382a465136
Weichun <weichun.cs08g@nctu.edu.tw>
2020-12-15 00:08:40 +0800

    update build script

--- .build.sh
+++ .build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-mkdir -p -m=700 cmake-build-dist
-pushd cmake-build-dist
-cmake -DCMAKE_C_COMPILER=$(which clang-11) -DCMAKE_CXX_COMPILER=$(which clang++-11) -DCMAKE_BUILD_TYPE=Release ../
-make -j8
-popd
+targetDir=cmake-build-dist
+
+if [ "$1" == "clean" ]; then
+    rm -rf $targetDir
+fi
+
+mkdir -p -m=700 $targetDir
+pushd $targetDir
+
+cmake \
+  -DCMAKE_C_COMPILER=$(which clang-11 clang | head -n1) \
+  -DCMAKE_CXX_COMPILER=$(which clang++-11 clang++ | head -n1) \
+  -DCMAKE_BUILD_TYPE=Release ../
+
+make -j$(nproc --all)
+
+if [ $? -eq 0 ]; then
+  binPath=$(realpath mjpeg_encoder)
+  echo -e "Build succeeded.\nExecutable Path: [$binPath]."
+fi
+
+popd > /dev/null
